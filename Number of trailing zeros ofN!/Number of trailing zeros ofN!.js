0ebfede9fff5a65392603d7164443ecfe3132f89
KAM9077 <kamalhajjar907@gmail.com>
2017-04-19 18:03:17 +0300

    modifying

--- Number of trailing zeros ofN!/Number of trailing zeros ofN!.js
+++ Number of trailing zeros ofN!/Number of trailing zeros ofN!.js
@@ -1,3 +1,5 @@
+//https://www.codewars.com/kata/52f787eb172a8b4ae1000a34/train/javascript
+
 // Write a program that will calculate the number of trailing zeros in a factorial of a given number.
 
 // http://mathworld.wolfram.com/Factorial.html
